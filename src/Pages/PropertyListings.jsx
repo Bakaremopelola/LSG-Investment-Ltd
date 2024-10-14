534e304c085f91b0110a2570bd1884ac63899d7d
Bakaremopelola <152390768+Bakaremopelola@users.noreply.github.com>
2024-10-14 15:40:34 +0100

    Update PropertyListings.jsx

--- src/Pages/PropertyListings.jsx
+++ src/Pages/PropertyListings.jsx
@@ -1,6 +1,6 @@
 import './PropertyListing.css'; // Add this CSS file for styling
-import Lsg1 from '../assets/Lsg1.jpg'; // Replace with real image paths
-import Lsg8 from '../assets/Lsg8.jpg';
+import lsg1 from '../assets/lsg1.jpg'; // Replace with real image paths
+import lsg8 from '../assets/lsg8.jpg';
 import maitama4 from '../assets/maitama4.jpg'; 
 import maitama5 from '../assets/maitama5.jpg'; // Replace with real image paths
 
