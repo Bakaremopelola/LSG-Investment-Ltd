a2a35d7c487ab7bd1167ed467245697f0e39e35e
Bakaremopelola <152390768+Bakaremopelola@users.noreply.github.com>
2025-01-13 17:44:27 +0100

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,6 +2,9 @@ import { useState, useRef } from 'react';
 import videoUrl from '../assets/vid.mp4';
 // import videoUrl from "https://drive.google.com/file/d/1QhwrMIOeTUZ2a7F9hcSElmtYhEHiRQkR/view?usp=drive_link"
 import './Hero.css';
+ const youtubeUrl = `https://www.youtube.com/watch?v=Hqi-H_47yfY`;
+
+
 
 const Hero = () => {
   const [isMuted, setIsMuted] = useState(true); // Mute by default
@@ -18,7 +21,7 @@ const Hero = () => {
   return (
     <div className="hero-container">
       {/* Background video */}
-       <video
+{/*        <video
         ref={videoRef}
         className="background-video"
         src={videoUrl}
@@ -27,7 +30,17 @@ const Hero = () => {
         muted={isMuted} // Bind mute state to video
         controls // Optional: Adds play/pause controls
         style={{ width: "100%", height: "auto" }} // Make responsive
-      />
+      /> */}
+         <iframe
+        width="100%"
+        height="100%"
+        src={youtubeUrl}
+        title="YouTube video"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        style={{ border: "none" }}
+      ></iframe>
       {/* Hero content with mute button */}
       <div className="hero-content">
         <button className="mute-btn" onClick={toggleMute}>
