c2ddbe86ddb78f60a23273720e19a8435207bdff
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2025-04-03 18:02:21 +0100

    homepage resolved

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -17,18 +17,18 @@ const Banner = () => {
     <div className="bg-gradient-to-r from-yellow-500 to-green-500 px-6 lg:px-40 py-6 flex flex-col md:flex-row justify-between items-center text-white rounded-lg shadow-lg">
       {/* Text Section */}
       <div className="text-center md:text-left mb-4 md:mb-0">
-        <h2 className="text-lg font-bold">Ready to Buy, Sell, or Invest?</h2>
-        <p className="text-sm max-w-md">
+        <h2 className="text-3xl lg:text-lg font-bold">Ready to Buy, Sell, or Invest?</h2>
+        <p className=" text-xl text-blue-950 lg:text-white lg:text-sm max-w-md">
           Register now to access verified land deals, connect with opportunities, and get the support you need. Whether you’re a potential buyer or an ambitious realtor.
         </p>
       </div>
 
       {/* Buttons */}
-      <div className="flex space-x-4">
-        <button onClick={handleClick} className="bg-blue-900 text-white px-5 py-2 rounded-lg text-sm font-medium hover:bg-blue-700 transition">
+      <div className="flex space-x-4 flex-col md:flex-rows gap-4">
+        <button onClick={handleClick} className="bg-blue-900 text-white px-5 py-2 rounded-4xl text-xl font-medium hover:bg-blue-700 w-[80vw] h-[4em] transition">
           Get a FREE Quote
         </button>
-        <button onClick={handleSubmit} className="border border-white text-white px-5 py-2 rounded-lg text-sm font-medium hover:bg-white hover:text-green-700 transition">
+        <button onClick={handleSubmit} className="border border-blue-950 text-blue-950 lg:text-white px-5 py-2 rounded-4xl text-xl w-[80vw] lg:w-auto h-[4em] lg:h-auto font-medium hover:bg-white hover:text-green-700 transition">
           Partner with Us
         </button>
       </div>
